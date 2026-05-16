1390178c7e01e4597a33b58e70d8faa5f45fe7c1
Asad <syedasadabbas.1815@gmail.com>
2026-05-16 15:20:39 +0500

    Initial commit with proper .gitignore

---
+++ temp_orders_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../../widgets/admin_drawer.dart';
+import '../../../services/order_service.dart';
+import '../../../models/order.dart';
+
+class OrdersScreen extends StatefulWidget {
+  // ... (existing code)
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  // ... (rest of the existing code)
+}
